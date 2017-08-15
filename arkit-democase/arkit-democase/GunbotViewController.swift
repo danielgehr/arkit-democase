2dd5358bf258e640be2e5c252b744723e2a03187
Gehr Daniel, GHR-DEV-VOT-R2-CH2 <Daniel.Gehr1@swisscom.com>
2017-08-15 15:36:03 +0200

    add new gunbot view controller

---
+++ arkit-democase/arkit-democase/GunbotViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GunbotViewController.swift
+//  arkit-democase
+//
+//  Created by Gehr Daniel, GHR-DEV-VOT-R2-CH2 on 15.08.17.
+//  Copyright © 2017 Swisscom. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class GunbotViewController: UIViewController {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
